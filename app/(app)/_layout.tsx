0d33a94faf416b99e7fe1975990ac6cbdfac3232
Emanuel <emanuel.snasc1@gmail.com>
2024-11-09 23:53:11 -0300

    create app nested route

---
+++ app/(app)/_layout.tsx
@@ -0,0 +1,33 @@
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { Text } from "react-native";
+import { Redirect, Slot } from "expo-router";
+import BaseLayout from "@/components/BaseLayout";
+import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
+
+export default function AppLayout() {
+	/*
+		const { session, isLoading } = useSession();
+	
+		// You can keep the splash screen open, or render a loading screen like we do here.
+		if (isLoading) {
+		  return <Text>Loading...</Text>;
+		}
+	  
+		// Only require authentication within the (app) group's layout as users
+		// need to be able to access the (auth) group and sign in again.
+		if (!session) {
+		  // On web, static rendering will stop here as the user is not authenticated
+		  // in the headless Node process that the pages are rendered in.
+		  return <Redirect href="/sign-in" />;
+		}
+	*/
+	return (
+		<GestureHandlerRootView>
+			<BottomSheetModalProvider>
+				<BaseLayout>
+					<Slot />
+				</BaseLayout>
+			</BottomSheetModalProvider>
+		</GestureHandlerRootView>
+	);
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,21 +1,11 @@
-import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Slot } from "expo-router";
-import BaseLayout from "@/components/BaseLayout";
-import { BottomSheetModalProvider } from "@gorhom/bottom-sheet";
 import { AppContextProvider } from "@/context/AppContext";
 
 export default function RootLayout() {
 
 	return (
-		
-		<GestureHandlerRootView>
-			<BottomSheetModalProvider>
-				<AppContextProvider>
-					<BaseLayout>
-						<Slot />
-					</BaseLayout>
-				</AppContextProvider>
-			</BottomSheetModalProvider>
-		</GestureHandlerRootView>
+		<AppContextProvider>
+			<Slot />
+		</AppContextProvider>
 	);
 }
